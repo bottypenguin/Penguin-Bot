ce6d3636253a648e3f535ad114cf145eea60ff8f
bottypenguin <78790324+bottypenguin@users.noreply.github.com>
2021-03-01 16:01:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,409 @@
+import os
+import re
+import discord
+from discord.ext import commands
+from datetime import datetime, timedelta
+import params_rs as params
+from redstar import Rs
+from keep_awake import keep_awake # used to keep the server awake otherwise it goes to sleep after 1h of inactivity
+import dotenv
+
+#intents = discord.Intents.default()
+#intents.members = True
+
+bot = discord.ext.commands.Bot(command_prefix=['!']) #, intents=intents)
+bot.remove_command('help')
+bot_ready = True
+dbg_ch = bot.get_channel(params.SERVER_DEBUG_CHANNEL_ID)
+
+# Help command  
+
+@bot.command(name='help', help='general help page', aliases=params.help_aliases)
+async def cmd_help(ctx: discord.ext.commands.Context):
+    """
+    General help command
+    :param ctx:
+    :return:
+    """
+    # standard handling of commands
+    await ctx.message.delete(delay=params.MSG_DELETION_DELAY)
+    print(f'cmd_help(): called by {ctx.author} using "{ctx.message.content}" in #{ctx.channel.name}')
+
+    # last_help_message = Last_help_message
+    last_help_message = None
+
+    if last_help_message is not None:
+        await last_help_message.delete()
+
+    embed = discord.Embed(color=params.EMBED_COLOR)
+    embed.set_author(name='RS Queue Help',
+                      icon_url=params.BOT_DISCORD_ICON)
+    embed.set_footer(text=f'Called by {ctx.author.display_name}\nDeleting in {params.HELP_DELETION_DELAY} sec')
+    embed.add_field(name="`!in`",
+                    value="Sign up for your highest RS level.",
+                    inline=False)
+    embed.add_field(name="`!in X [note]`",
+                    value="Sign up for RS level X (optional: with note).",
+                    inline=False)
+    embed.add_field(name="`!out`", value="Leave all queues.", inline=False)
+    embed.add_field(name="`!out X`",
+                    value="Leave queue of RS level X.",
+                    inline=False)
+    embed.add_field(name="`!q`",
+                    value="Display running queues.",
+                    inline=False)
+    embed.add_field(name="`!start X`",
+                    value="Early start RS level X queue.",
+                    inline=False)
+    embed.add_field(name="`!clear X`",
+                    value="Clear queue for RS level X.",
+                    inline=False)
+    last_help_message = await ctx.channel.send(embed=embed)
+    await last_help_message.delete(delay=params.HELP_DELETION_DELAY)
+
+    globals()['Last_help_message'] = last_help_message
+
+@bot.command(name='rsstats', help='RS statistics', aliases=params.rs_stats_aliases)
+
+async def cmd_rs_stats(ctx: discord.ext.commands.Context):
+    """
+    Display RS statistics
+    :param ctx: discord context
+    :return:
+    """
+
+    # standard handling of commands
+    await ctx.message.delete(delay=params.MSG_DELETION_DELAY)
+    print(f'cmd_rs_stats(): called by {ctx.author} using "{ctx.message.content}" in #{ctx.channel.name}')
+
+    embed = discord.Embed(color=params.EMBED_COLOR)
+    embed.set_author(name='RS Counter \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800   \u2800', icon_url= params.SERVER_DISCORD_ICON)
+    embed.set_footer(text=f'Called by {ctx.author.display_name}\nDeleting in {params.HELP_DELETION_DELAY} sec')
+
+    rs_chan = bot.get_channel(params.SERVER_RS_CHANNEL_ID)
+    text = f'Total Runs \n' # in {rs_chan.mention}
+    total = sum(Rs.stats.values())
+
+    for rs, cnt in Rs.stats.items():
+        lvl = int(rs.replace('RS', ''))
+        if lvl == params.SUPPORTED_RS_LEVELS_MAX +1 : break
+        text += f'**{rs}**: {cnt} _({round(cnt/total*100)}%)_\n'
+
+    embed.description = text
+    m = await ctx.send(embed=embed)
+    await m.delete(delay=params.HELP_DELETION_DELAY)
+
+@bot.command(name='rsrules', help='rsrules', aliases=params.rs_rules_aliases)  
+
+async def cmd_rs_rules(ctx: discord.ext.commands.Context):
+    await ctx.message.delete()
+
+    print(f'cmd_rs_rules(): requested by {ctx.author} in #{ctx.channel.name}')
+    text = params.TEXT_RULES
+    if params.TEXT_RULES_FORMAT == 'Message' :
+      channel = bot.get_channel(params.RULES_CHANNEL_ID)
+      message = await channel.fetch_message(params.RULES_MESSAGE_ID) #.content
+      text = message.content
+    embed = discord.Embed(color=params.EMBED_COLOR, delete_after = params.RULES_DELETION_DELAY)
+    embed.set_author(name= f'{params.TEXT_RULES_TITLE}', icon_url=params.SERVER_DISCORD_ICON)
+    embed.set_footer(text=f'Called by {ctx.author.display_name}\nDeleting in {int("{:.0f}".format(params.RULES_DELETION_DELAY/60))} min')
+
+    
+    embed.description = text
+    m = await ctx.send(embed=embed)
+    await m.delete(delay=params.RULES_DELETION_DELAY)
+
+@bot.command(name='enter', help='Join the RS queue', aliases=params.enter_queue_aliases)
+
+async def cmd_enter_rs_queue(ctx: discord.ext.commands.Context, level_arg: str = '0', *, comment: str = ''):
+    """
+    Join RS queue
+    :param comment:
+    :param ctx: discord context
+    :param level_arg: rs level(s) to join, separated by comma
+    :return:
+    """
+    # only handle rs commands in the rs channel
+    if ctx.message.channel.id != params.SERVER_RS_CHANNEL_ID:
+        return
+
+    async with ctx.channel.typing():
+
+        # standard handling of commands
+        await ctx.message.delete(delay=params.MSG_DELETION_DELAY)
+        print(f'cmd_enter_rs_queue(): called by {ctx.author} using "{ctx.message.content}" in #{ctx.channel.name}')
+
+        # relay command to module
+        levels = level_arg.replace(',', ' ').split(' ')
+        for level in levels:
+            Rs.add_job(Rs.enter_queue, [ctx.author, int(level), comment, False, False])
+
+@bot.command(name='leave', help='Leave the RS queue', aliases=params.leave_queue_aliases)
+
+async def cmd_leave_rs_queue(ctx: discord.ext.commands.Context, *, level_arg: str = '0'):
+    """
+    Leave RS queue(s)
+    :param ctx: discord context
+    :param level_arg: rs level(s) to join, separated by comma
+    :return:
+    """
+    # only handle rs commands in the rs channel
+    if ctx.message.channel.id != params.SERVER_RS_CHANNEL_ID:
+        return
+
+    async with ctx.channel.typing():
+
+        # standard handling of commands
+        await ctx.message.delete(delay=params.MSG_DELETION_DELAY)
+        print(f'cmd_leave_rs_queue(): called by {ctx.author} using "{ctx.message.content}" in #{ctx.channel.name}')
+
+        # relay command to module
+        levels = level_arg.replace(',', ' ').split(' ')
+        for level in levels:
+            Rs.add_job(Rs.leave_queue, [ctx.author, int(level), False, False, False, None])
+
+@bot.command(name='display', help='Display the current RS queues', aliases=params.display_queue_aliases)
+
+async def cmd_display_rs_queues(ctx: discord.ext.commands.Context):
+    """
+    Display current queues that contain at least one player
+    :param ctx: discord context
+    :param level: rs level to display
+    :return:
+    """
+    # only handle rs commands in the rs channel
+    if ctx.message.channel.id != params.SERVER_RS_CHANNEL_ID:
+        return
+
+    # standard handling of commands
+    await ctx.message.delete(delay=params.MSG_DELETION_DELAY)
+    print(f'cmd_display_rs_queues(): called by {ctx.author} using "{ctx.message.content}" in #{ctx.channel.name}')
+
+    # relay command to module
+    Rs.add_job(Rs.display_queues, [False])
+
+@bot.command(name='start', help='Start a queue early', aliases=params.start_queue_aliases)
+
+async def cmd_start_rs_queue(ctx: discord.ext.commands.Context, level: str):
+    """
+    Start a certain queue
+    :param ctx: discord context
+    :param level: rs level to start
+    :return:
+    """
+    # only handle rs commands in the rs channel
+    if ctx.message.channel.id != params.SERVER_RS_CHANNEL_ID:
+        return
+
+    # standard handling of commands
+    await ctx.message.delete(delay=params.MSG_DELETION_DELAY)
+    print(f'cmd_start_rs_queue(): called by {ctx.author} using "{ctx.message.content}" in #{ctx.channel.name}')
+
+    # relay command to module
+    Rs.add_job(Rs.start_queue, [ctx.author, int(level)])
+
+@bot.command(name='clear', help='Clear a queue', aliases=params.clear_queue_aliases)
+async def cmd_clear_rs_queue(ctx: discord.ext.commands.Context, level: str):
+    """
+    Clear a certain queue
+    :param ctx: discord context
+    :param level: rs level to start
+    :return:
+    """
+    # only handle rs commands in the rs channel
+    if ctx.message.channel.id != params.SERVER_RS_CHANNEL_ID:
+        return
+
+    # standard handling of commands
+    await ctx.message.delete(delay=params.MSG_DELETION_DELAY)
+    print(f'cmd_clear_rs_queue(): called by {ctx.author} using "{ctx.message.content}" in #{ctx.channel.name}')
+
+    # relay command to module
+    Rs.add_job(Rs.clear_queue, [ctx.author, int(level)])
+
+#################################
+# System commands module:
+#################################
+@bot.command(name='ping', help='ping')
+async def cmd_ping(ctx):
+    await ctx.message.delete(delay=params.MSG_DELETION_DELAY)
+    print(f'cmd_ping(): called by {ctx.author} using "{ctx.message.content}" in #{ctx.channel.name}')
+    await ctx.send(f'Pong! Latency = {round(bot.latency, 3)}s.')
+
+@bot.command(name='shutdown', help='persist current state and shutdown')
+@commands.has_permissions(administrator=True)
+async def shutdown(ctx):
+    pass
+
+@bot.command(name='settings', help='Debug')
+@commands.has_permissions(administrator=True)
+async def settings(ctx):
+    pass
+
+#################################
+# Bot events
+#################################
+@bot.event
+async def on_message(message):
+
+    # skip event if: msg from bot itself or debug_mode / debug_channel
+    if message.author == bot.user or message.guild.id != params.SERVER_DISCORD_ID:
+        return
+
+    # allow '+' and '-' as soft prefixes inside rs channel
+    if message.channel.id == params.SERVER_RS_CHANNEL_ID and message.content[0] in '+-':
+        message.content = '!' + message.content
+
+    # hack to understand commands like "! cmd" (ignore space)
+    if len(message.content) > 2 and message.content[0] == '!' and message.content[1] == ' ':
+        message.content = message.content.replace(' ', '', 1)
+
+    # hack to understand commands like "!in6" (ignore missing space)
+    if re.match('^![a-z]{1,3}[1-9]+', message.content):
+        message.content = message.content.replace('!in', '!in ', 1)
+
+    await bot.process_commands(message)
+
+
+@bot.event
+async def on_ready():
+    
+    # initialize all command modules
+    Rs.init(bot)        # red star
+
+    print(f'on_ready(): modules initialized')
+
+
+    # Clean dead embedds
+    mgs = [] #Empty list to put all the messages in the log
+    channel = bot.get_channel(params.SERVER_RS_CHANNEL_ID) 
+
+    async for message in channel.history(limit=100):
+      if message.author == bot.user :
+        mgs.append(message)
+    await channel.delete_messages(mgs)
+
+    
+    # Clean logs older than 24h
+    mgs = [] #Empty list to put all the messages in the log
+    channel = bot.get_channel(params.SERVER_DEBUG_CHANNEL_ID) 
+    time_differennce = timedelta(hours=24)
+    init_time = datetime.now()
+    
+    async for message in channel.history(limit=100):
+      if init_time - message.created_at > time_differennce and message.author == bot.user :
+        mgs.append(message)
+    await channel.delete_messages(mgs) 
+    mgs = [] #Empty list to avoid trouble with other code  
+      
+    # launch loop tasks
+    if not Rs.task_process_queue.is_running() and params.DEBUG_MODE is False:
+        Rs.task_process_queue.start()
+        print(f'on_ready(): launching Rs.task_process_queue...')
+    if not Rs.task_check_afk.is_running() and params.DEBUG_MODE is False:
+        Rs.task_check_afk.start()
+        print(f'on_ready(): launching Rs.task_check_afk...')
+    if not Rs.task_repost_q.is_running() and params.DEBUG_MODE is False:
+        Rs.task_repost_q.start()
+        print(f'on_ready(): launching Rs.task_repost_q...')
+
+    # other stuff
+    if not params.DEBUG_MODE:
+        Rs.add_job(Rs.display_queues, [True])
+
+    global bot_ready
+    bot_ready = True
+    print(f'on_ready(): {bot.user.name} is ready')
+    dbg_ch = bot.get_channel(params.SERVER_DEBUG_CHANNEL_ID)
+    await dbg_ch.send(
+        f'**INFO**: on_ready(): Initialization complete')
+
+
+@bot.event
+async def on_connect():
+    print(f'on_connect(): {bot.user.name} has connected')
+    
+    if params.DEBUG_MODE is False:
+        if dbg_ch:
+            await dbg_ch.send(
+            f'**INFO**: on_connect(): Connection (re-)established')
+
+@bot.event
+async def on_disconnect():
+    print(f'on_disconnect(): {bot.user.name} has disconnected')
+    await dbg_ch.send(f':warning: **ERROR**: on_disconnect(): Connection lost')
+
+
+@bot.event
+async def on_resumed():
+    print(f'on_resumed(): {bot.user.name} has resumed')
+    # if params.DEBUG_MODE is False:
+    #     
+    #     await dbg_ch.send(
+    #         f'**INFO**: on_resumed(): Connection recovered')
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+
+    global bot_ready
+    if not bot_ready:
+        return
+
+    # early catch for own reactions
+    if user.id == bot.user.id:
+        return
+
+    # print(f'on_reaction_add(): {reaction.emoji} by {user}')
+
+    chan_id = reaction.message.channel.id
+
+    # DEBUG
+    if chan_id == params.SERVER_DEBUG_CHANNEL_ID:
+        await Rs.handle_reaction(user, reaction)
+        return
+
+    # RS reactive queue system
+    if chan_id == params.SERVER_RS_CHANNEL_ID:
+        await Rs.handle_reaction(user, reaction)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+
+    print(f'on_command_error(): {error}')
+    print(f'on_command_error(): message was: "{ctx.message.content}" sent by {ctx.author.name} in #{ctx.channel.name}')
+
+    
+    print(f'**ERROR**\n{error}\n**REASON**\n"{ctx.message.content}" sent by {ctx.author.name} in #{ctx.channel.name}')
+
+    try:
+        await ctx.message.add_reaction('🤖')
+        await ctx.message.add_reaction('❔')
+    except discord.errors.NotFound:
+        print(f'on_command_error(): message was deleted -> no reaction')
+    except Exception:
+        pass
+
+    if isinstance(error, commands.errors.CheckFailure):
+        m = await ctx.send(f'{ctx.author.mention} You do not have the correct role for this command!')
+        await m.delete(delay=params.MSG_DISPLAY_TIME)
+    elif isinstance(error, commands.errors.MissingPermissions):
+        m = await ctx.send(f'{ctx.author.mention} I\'m missing permissions to do that!')
+        await m.delete(delay=params.MSG_DISPLAY_TIME)
+
+
+# kill handler to trigger a clean exit (e.g. to clear up any open chat clutter)
+def handle_exit():
+    pass
+
+# kill handler -> cleanup before exiting
+# atexit.register(handle_exit)
+# signal.signal(signal.SIGTERM, handle_exit)
+# signal.signal(signal.SIGINT, handle_exit)
+
+# start
+keep_awake()
+print('main: connecting...')
+dotenv.load_dotenv(verbose=True)
+bot.run(os.getenv("DISCORD_TOKEN"))
---
+++ params_rs.py
@@ -0,0 +1,190 @@
+###################################################
+# DISCORD                                         #
+###################################################
+DEBUG_MODE = False
+SPLIT_CHANNELS = False
+
+SERVER_DEBUG_CHANNEL_ID = 809062902967435274 #log
+SERVER_BUG_CHANNEL_ID = 809062936416878622 #bug reporting
+SERVER_DISCORD_ID = 382153815073488898
+SERVER_DISCORD_NAME = ''
+SERVER_DISCORD_ICON = 'https://cdn.discordapp.com/avatars/808651447788240947/c63129c514d7cf1d1b6528969fcb06e6.png?size=1024'
+
+BOT_DISCORD_ICON = SERVER_DISCORD_ICON
+
+QUEUE_EMBED_ICON = ''
+QUEUE_EMBED_TITLE = ''
+
+BOT_DISCORD_ICON = SERVER_DISCORD_ICON
+RS_ICON = '<:StarRedStar:512096841056124960>'
+
+# CHANNEL IDs
+SERVER_RS_CHANNEL_ID = 508782999093903373 #809062936416878622
+
+###################################################
+# TIME CONSTANTS (in sec)                         #
+###################################################
+TIME_BOT_AFK_TASK_RATE = 50
+TIME_BOT_Q_TASK_RATE = 30
+
+TIME_SPAM_BRAKE = 1
+TIME_AFK_WARN = 60 * 10  # afk warning as ping; afk_flag set in checker task after warning!
+TIME_AFK_KICK = 60 * 15  # kick if warning ignored. must be bigger than TIME_AFK_WARN!
+TIME_Q_REPOST = 3
+TIME_Q_REPOST_COOLDOWN = 1.2
+MSG_DELETION_DELAY = 7
+RULES_DELETION_DELAY = 60 * 3
+HELP_DELETION_DELAY = 30
+MSG_DISPLAY_TIME = 3
+INFO_DISPLAY_TIME = 60 * 3 # An Empty Embedd, Meet Where
+PING_COOLDOWN = 60 * 5  # time that has to pass before ping_all_role can be mentioned again
+
+###################################################
+# ROLES                                           #
+###################################################
+SUPPORTED_RS_LEVELS_MIN = 6
+SUPPORTED_RS_LEVELS_MAX = 10
+
+SUPPORTED_RS_LEVELS = range(SUPPORTED_RS_LEVELS_MIN, SUPPORTED_RS_LEVELS_MAX + 1)
+
+SERVER_MEMBER_ROLE = 'rs'
+SERVER_MEMBER_ROLE_ID = 760976687068217375
+SERVER_RS_ROLE_ID = SERVER_MEMBER_ROLE_ID
+
+SERVER_MODERATOR_ROLE = 'interim officer'
+SERVER_MODERATOR_ROLE_ID = 771906991140503574
+
+SERVER_ADMIN_ROLES = 'admin'
+SERVER_ADMIN_ROLE_ID = 418727225471270913
+
+SERVER_MEMBER_ROLES = ['rs-access']
+SERVER_ALLY_ROLES = ['rs-access']
+SERVER_MODERATOR_ROLES = ['interim officer', 'Officer']
+SERVER_ADMIN_ROLES = ['admin','Executive']
+
+RS4_ROLE = 'RS4'
+RS5_ROLE = 'RS5'
+RS6_ROLE = 'RS6'
+RS7_ROLE = 'RS7'
+RS8_ROLE = 'RS8'
+RS9_ROLE = 'RS9'
+RS10_ROLE = 'RS10'
+RS11_ROLE = 'RS11'
+
+RS_ROLES = [RS4_ROLE, RS5_ROLE, RS6_ROLE, RS7_ROLE, RS8_ROLE, RS9_ROLE, RS10_ROLE, RS11_ROLE]
+
+RESTRICTING_ROLES =  ['no4','no5','no6','no7','no8','no9','no10','no11'] # for players who can't make it in given RS level
+
+SERVER_PING_ROLES = ['rs4','rs5','rs6','rs7','rs8','rs9','rs10','rs11'] # 1/4 2/4 3/4 4/4
+SERVER_SOFT_PING_ROLES = SERVER_PING_ROLES
+# ['rs4s','rs5s','rs6s','rs7s','rs8s','rs9s','rs10s','rs11s'] # 3/4 4/4
+SERVER_SOFT_NO_ROLES = SERVER_PING_ROLES
+# ['rs4n','rs5n','rs6n','rs7n','rs8n','rs9n','rs10n','rs11n'] # 4/4
+
+OLD_STARS = {'RS6' : 214, 'RS7' : 320, 'RS8' : 1409, 'RS9' : 3131, 'RS10' : 10}
+
+###################################################
+# COMMAND ALIASES                                 #
+###################################################
+help_aliases = ['h', 'H', 'Help']
+
+# RS
+rs_help_aliases = ['rsh']
+rs_aliases = ['rsc', 'Rs', 'rS', 'RS', 'RSC', 'Rsc']
+rs_stats_aliases = ['st', 't']
+rs_rules_aliases = ['r', 'rsr', 'rsrule', 'rule', 'rules']
+display_queue_aliases = ['q', 'Q', 'queue', 'Queue']
+enter_queue_aliases = ['i', 'I', 'in', 'In', 'IN', 'iN', 'join', 'Join']
+leave_queue_aliases = ['o', 'O', 'out', 'Out', 'OUT']
+start_queue_aliases = ['s', 'S', 'Start']
+clear_queue_aliases = ['c', 'C', 'Clear']
+rs_clean_aliases = []
+
+###################################################
+# APPEARANCE                                      #
+###################################################
+EMBED_COLOR = 0x43b581
+QUEUE_EMBED_COLOR = 0x2f3136
+
+# rs emojis
+RS0_EMOJI = "0️⃣"
+RS1_EMOJI = "1️⃣"
+RS2_EMOJI = "2️⃣"
+RS3_EMOJI = "3️⃣"
+RS4_EMOJI = "4️⃣"
+RS5_EMOJI = "5️⃣"
+RS6_EMOJI = "6️⃣"
+RS7_EMOJI = "7️⃣"
+RS8_EMOJI = "8️⃣"
+RS9_EMOJI = "9️⃣"
+RS10_EMOJI = "🔟"
+RS11_EMOJI = "⏸️"
+# RS11_EMOJI = "<:eleven:760869824036601939>"
+# RS11_EMOJI_ID = 760869824036601939
+
+RS_EMOJIS =  [RS4_EMOJI, RS5_EMOJI, RS6_EMOJI, RS7_EMOJI, RS8_EMOJI, RS9_EMOJI, RS10_EMOJI, RS11_EMOJI]
+
+# dialogues
+JOIN_EMOJI = "🆕"
+LEAVE_EMOJI = "❎"
+START_EMOJI = "▶"
+CONFIRM_EMOJI = "✅"
+CANCEL_EMOJI = "❎"
+GO_BACK_EMOJI = "🔙"
+DOWN_EMOJI = "⬇" 
+
+PING_THRESHOLDS = [1, 2, 3] # ping at these queue sizes only
+
+MAX_RS_NOTE_LENGTH = 50
+
+MAX_RS_NOTE_LENGTH = 50
+
+MAX_RS_NOTE_LENGTH = 50
+
+MAX_RS_NOTE_LENGTH = 50
+
+# TEXTS 
+
+RULES_CHANNEL_ID = 525599595812880384
+RULES_MESSAGE_ID = 574195487171805333
+RULES_MESSAGE_ID2 = 574195496835350528
+RULES_MESSAGE_ID_FR = 805569774822096916
+
+TEXT_EMPTY_QUEUE = 'Start a new queue by typing `!in` or reacting below!\n' \
+                                'Questions? `!help`, Hide/Restore channel: `!rs`\n' \
+                                f'Bugs or Ideas? Please report them in '
+
+TEXT_RULES_FORMAT = 'Text'
+TEXT_RULES_TITLE = 'RS QUEUE ACCESS, TERMS OF SERVICE'
+
+TEXT_RULES_EN = f"**1.Dont Grief** \
+I think this goes without saying, when running RS runs with us, you shouldn't be doing it with the intent to disrupt anyones gameplay. \
+\n\
+2.**Cooperate With Everyone** \
+This should be obvious as well. The game is designed to reward teamwork. To ensure the smoothest run possible, you must communicate with other players - members of the Penguin Horde or not. This means that: \n\
+•You will ping planet sectors before engaging them, wait and listen for any concerns other players may express. Also, pay attention to the chat at all times. \
+•Make sure you and your team pick planets centralised, that are accessible to everyone, and have enough artifacts to be worthwhile (unless you decide on otherwise). \
+•If you have anything to suggest during the run, say it! This is about teamwork after all, and you are part of the team. \n\
+•Try not to abandon the run, unless forced to of course by any unforeseen circumstances. \
+3. **This service is for in-corp RS runs** \
+This means that you will have to join a corporation of the Penguin Horde, that will be agreed upon by the member users. This rules do not apply to club runs hosted by Penguin Horde.\
+\n\
+4. **Be civil at all times** \
+If you come to any disagreements with anyone during your RS run, before it or after it, please be civil. Anything can be solved with mature discussion. \
+\n\
+5. **If you break any of the above terms** \
+Really, it depends on the severity of your action. There may not even be a warning, or you may even loose access to this service or get banned from the server overall. Know that you have absolutely nothing to worry about if all you plan on is getting some arts and enjoying a RS run with some good company. \
+\n\
+Now, i know some of you may have skipped down here to just find the command that will give you access without reading the above conditions. Please read them, so we dont have any misunderstandings. \n\
+Anyways, use !agree to get access to the RS queue. \
+If at any point you wish to no longer have access, use !leave. We are happy you are choosing to run Red Stars with us!"
+TEXT_RULES = TEXT_RULES_EN
+
+TEXT_NOROLESET = "you didn't select the"
+TEXT_MEET_WERE = ''
+
+# @zofia: careful with mobile version of discord!
+# @BenSmith30: I think this is exactly what fits on mobile... No?
+#+ DOWN_EMOJI +'Join.Unirse.Вступать.加入 Quit.Dejar.Уехать.退出'+ LEAVE_EMOJI
+
+TEXT_FOOTER_TEXT = '\u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800 \u2800   \u2800' # Zofia' magic works on mobile and pc
